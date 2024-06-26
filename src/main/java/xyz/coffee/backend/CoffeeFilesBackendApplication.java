e0bc6b29534bcb6f4a5090fc2118558dcb422a70
Martin Alexander Ramos Yampufé <alexanderamsyam@gmail.com>
2024-06-26 18:42:08 -0500

    FEAT: Load .sav file content as String with TheCupsOfCoffee .sav file format

--- src/main/java/xyz/coffee/backend/CoffeeFilesBackendApplication.java
+++ src/main/java/xyz/coffee/backend/CoffeeFilesBackendApplication.java
@@ -1,11 +1,98 @@
 package xyz.coffee.backend;
 
+import java.nio.ByteBuffer;
+import java.nio.file.Path;
+import java.time.LocalDateTime;
+import java.util.Base64;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+// Temp
+import org.springframework.context.annotation.Bean;
+
+import xyz.coffee.backend.service.auth.api.interfaces.SavFileWriter;
+import xyz.cupscoffee.files.api.implementation.BasicDisk;
+import xyz.cupscoffee.files.api.implementation.BasicFile;
+import xyz.cupscoffee.files.api.implementation.BasicFolder;
+import xyz.cupscoffee.files.api.implementation.BasicSavFile;
+
 @SpringBootApplication
 public class CoffeeFilesBackendApplication {
-	public static void main(String[] args) {
-		SpringApplication.run(CoffeeFilesBackendApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(CoffeeFilesBackendApplication.class, args);
+    }
+
+    // Temp
+    @Autowired
+    private SavFileWriter savFileWriter;
+
+    @Bean
+    CommandLineRunner init() {
+        return args -> {
+            String fileContent = "console.log('Hello, World!');\n";
+            Base64.Encoder encoder = Base64.getEncoder();
+            String encodedFileContent = encoder.encodeToString(fileContent.getBytes());
+            ByteBuffer fileContentBuffer = ByteBuffer.wrap(encodedFileContent.getBytes());
+            Map<String, String> metadata = new java.util.HashMap<>();
+            metadata.put("author", "Elder");
+
+            BasicFile basicFile = new BasicFile("archivo.js",
+                    fileContentBuffer,
+                    LocalDateTime.now(),
+                    LocalDateTime.now(),
+                    fileContentBuffer.capacity(),
+                    Path.of("A:\\directorio\\otro_directorio\\archivo.js"),
+                    metadata);
+            BasicFolder basicFolder1 = new BasicFolder("otro_directorio",
+                    List.of(basicFile),
+                    new LinkedList<>(),
+                    LocalDateTime.now(),
+                    LocalDateTime.now(),
+                    fileContentBuffer.capacity(),
+                    Path.of("A:\\directorio\\otro_directorio"),
+                    metadata);
+            BasicFolder basicFolder2 = new BasicFolder("directorio",
+                    new java.util.LinkedList<>(),
+                    new LinkedList<>(),
+                    LocalDateTime.now(),
+                    LocalDateTime.now(),
+                    fileContentBuffer.capacity(),
+                    Path.of("A:\\directorio\\directorio"),
+                    metadata);
+            BasicFolder basicFolder3 = new BasicFolder("directorio",
+                    new java.util.LinkedList<>(),
+                    List.of(basicFolder1, basicFolder2),
+                    LocalDateTime.now(),
+                    LocalDateTime.now(),
+                    fileContentBuffer.capacity(),
+                    Path.of("A:\\directorio"),
+                    metadata);
+            BasicFolder root = new BasicFolder("",
+                    new java.util.LinkedList<>(),
+                    List.of(basicFolder3),
+                    LocalDateTime.now(),
+                    LocalDateTime.now(),
+                    fileContentBuffer.capacity(),
+                    Path.of("A:\\"),
+                    metadata);
+
+            BasicDisk basicDisk = new BasicDisk("A",
+                    root,
+                    root.getSize() * 3,
+                    root.getSize(),
+                    new java.util.HashMap<>());
+
+            BasicSavFile savFile = new BasicSavFile("CupsOfCoffee",
+                    new BasicDisk[] { basicDisk },
+                    new java.util.HashMap<>());
+                    
+            savFileWriter.write(savFile);
+        };
+    }
 }
--- src/main/java/xyz/coffee/backend/service/auth/api/implementation/SavFileWriterImpl.java
+++ src/main/java/xyz/coffee/backend/service/auth/api/implementation/SavFileWriterImpl.java
@@ -1,16 +1,101 @@
 package xyz.coffee.backend.service.auth.api.implementation;
 
+import java.nio.charset.StandardCharsets;
+import java.time.LocalDateTime;
+import java.time.ZoneOffset;
+import java.util.List;
+
 import org.springframework.stereotype.Service;
 
-import xyz.coffee.backend.service.auth.api.interfaces.SavFileWriter;
+import io.jsonwebtoken.lang.Arrays;
+import xyz.cupscoffee.files.api.Disk;
+import xyz.cupscoffee.files.api.File;
+import xyz.cupscoffee.files.api.Folder;
+import xyz.cupscoffee.files.api.Metadata;
 import xyz.cupscoffee.files.api.SavFile;
+
+import xyz.coffee.backend.service.auth.api.interfaces.SavFileWriter;
 
 @Service
 public class SavFileWriterImpl implements SavFileWriter {
 
     @Override
     public void write(SavFile savFile) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'write'");
+        StringBuilder sb = new StringBuilder();
+
+        // Get the header
+        sb.append(savFile.getHeader() + "\n");
+
+        // Write the disks
+        Disk[] disks = savFile.getDisks();
+        for (int i = 0; i < disks.length; i++) {
+            Disk disk = disks[i];
+            sb.append(disk.getName() + ":");
+            loadFolderAsString(disks[i].getRootFolder(), sb);
+
+            if (i < disks.length - 1) {
+                sb.append("\n");
+            }
+        }
+    }
+
+    private void loadFolderAsString(Folder folder, StringBuilder sb) {
+        sb.append("|" + folder.getName() + "<");
+
+        // Load folders
+        List<Folder> subFolders = folder.getFolders();
+        for (Folder subFolder : subFolders) {
+            loadFolderAsString(subFolder, sb);
+        }
+
+        // Load files
+        List<File> files = folder.getFiles();
+        for (File file : files) {
+            loadFileAsString(file, sb);
+        }
+
+        sb.append(">");
+
+        // Load metadata
+        loadMetadataAsString(folder, sb);
+    }
+
+    private void loadFileAsString(File file, StringBuilder sb) {
+        sb.append("*" + file.getName() + "{");
+
+        String content = new String(file.getContent().array(), StandardCharsets.UTF_8);
+        ;
+        sb.append(content);
+
+        sb.append("}");
+
+        // Load metadata
+        loadMetadataAsString(file, sb);
+    }
+
+    private void loadMetadataAsString(Metadata metadata, StringBuilder sb) {
+        sb.append("[");
+
+        Arrays.asList(Metadata.class.getMethods()).forEach(method -> {
+            if (method.getName().startsWith("get") && !method.getName().equals("getClass")) {
+                try {
+                    sb.append(method.getName().substring(3) + ":");
+
+                    if (method.getReturnType().equals(LocalDateTime.class)) {
+                        LocalDateTime localDateTime = (LocalDateTime) method.invoke(metadata);
+                        sb.append(localDateTime.toEpochSecond(ZoneOffset.of("Z")) + ",");
+                        return;
+                    }
+
+                    sb.append(method.invoke(metadata) + ",");
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+
+        sb.deleteCharAt(sb.length() - 1);
+
+        sb.append("]");
     }
 }
