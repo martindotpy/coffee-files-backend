9d5487fc3d5fc617e15db7ea3a58074f8b8a0784
Martin Alexander Ramos Yampufé <alexanderamsyam@gmail.com>
2024-07-01 00:30:36 -0500

    FEAT: Add Builder pattern

--- src/main/java/xyz/cupscoffee/backend/api/response/ReadFileContentResponse.java
+++ src/main/java/xyz/cupscoffee/backend/api/response/ReadFileContentResponse.java
@@ -1,8 +1,10 @@
 package xyz.cupscoffee.backend.api.response;
 
+import lombok.Builder;
 import lombok.Getter;
 
 @Getter
+@Builder
 public class ReadFileContentResponse {
     private String content;
 }
--- src/main/java/xyz/cupscoffee/backend/api/response/SystemStatusResponse.java
+++ src/main/java/xyz/cupscoffee/backend/api/response/SystemStatusResponse.java
@@ -1,9 +1,11 @@
 package xyz.cupscoffee.backend.api.response;
 
+import lombok.Builder;
 import lombok.Getter;
 import xyz.cupscoffee.backend.api.squema.enums.SystemStatus;
 
 @Getter
+@Builder
 public class SystemStatusResponse {
     private SystemStatus status;
 }
